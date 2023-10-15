2ad65d6655fc9be6aaa618095dcc0b40f5b17f1b
Dheeraj Shrestha <dheeraj@crossovernepal.com>
2023-10-15 22:58:21 +0545

    first commit

---
+++ Finder/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Finder.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finder.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
---
+++ Finder/Entities/AppUser.cs
@@ -0,0 +1,8 @@
+﻿namespace Finder.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
