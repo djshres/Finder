161f012a33d4e63b9acd2cbd067cf82f00146cfc
Dheeraj Shrestha <dheeraj@crossovernepal.com>
2023-10-17 12:40:05 +0545

    Services, template forms, observables, async pipes, pipes, structural directive, component communication

---
+++ ClientSPA/src/app/_services/account.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { User } from '../_models/user';
+
+//Angular service is singleton. This is intiantiated when our application starts and destroyed when application stops.
+//This is different from component because component is destroyed when we move from one component to other.
+//Service is available for lifetime of the application so is good place to store state when required.
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = 'https://localhost:5001/api/';
+  private currentUserSource = new BehaviorSubject<User | null>(null);
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  login(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map(user => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+        // return user; //If we need the response in component return here
+      }) 
+    )
+  } 
+
+  setCurrentUser(user: User) {
+    this.currentUserSource.next(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
+  }
+}
---
+++ ClientSPA/src/app/nav/nav.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
+
+
+//Observables:
+//Are the lazy collections of multiple values over time
+//You can think of observables like a newsletter: Only subscriber of the newsletter receive the newsletter, If no-one subscribes the
+//newsletter it probably will not be printed
+//Emits multiple value over time, Lazy, able to cancel, Can use with map-filter-reduce-and other operators
+
+//Promise: 
+//Provides a single future value, not lazy, cannot cancel
+
+//Observavles and rxJs: 
+//If we wanna transform the data coming from api eg: map, pipe
+
+//Async Pipe: 
+//Automatically subscribes/unsubscribes from the observables
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+  // loggedIn = false;
+  // currentUser$: Observable<User | null> = of(null);
+
+  constructor(public accountService: AccountService) { }
+
+  ngOnInit(): void {
+    //this.getCurrentUser();
+    // this.currentUser$ = this.accountService.currentUser$;
+  }
+
+  //Note:In case of http request we don't need to unsubscribe, but in this case of our own observable we have to unsubscribe
+  //So use of aync pipe is the best option in this case
+  // getCurrentUser() {
+  //   this.accountService.currentUser$.subscribe({
+  //     next: user => this.loggedIn == !!user,
+  //     error: error => console.log(error)
+  //   })
+  // }
+
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        // this.loggedIn = true;
+      },
+      error: error => {
+        console.log(error);
+      }
+    })
+  }
+
+  logout() {
+    this.accountService.logout();
+    // this.loggedIn = false;
+  }
+}
