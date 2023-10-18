e49124114be28435b9bd6c5cf89388329f6763da
Dheeraj Shrestha <dheeraj@crossovernepal.com>
2023-10-18 09:52:13 +0545

    Angular routing, route guards, shared module

--- ClientSPA/src/app/nav/nav.component.ts
+++ ClientSPA/src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 
 //Observables:
@@ -29,7 +31,8 @@ export class NavComponent implements OnInit {
   // loggedIn = false;
   // currentUser$: Observable<User | null> = of(null);
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router,
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
     //this.getCurrentUser();
@@ -45,20 +48,29 @@ export class NavComponent implements OnInit {
   //   })
   // }
 
-  login() {
+  // login() {
+  //   this.accountService.login(this.model).subscribe({
+  //     next: response => {
+  //       // console.log(response);
+  //       // this.loggedIn = true;
+  //       this.router.navigateByUrl('/members');
+  //     },
+  //     error: error => {
+  //       console.log(error);
+  //     }
+  //   })
+  // }
+
+  login(){
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-        // this.loggedIn = true;
-      },
-      error: error => {
-        console.log(error);
-      }
+      next: _ =>  this.router.navigateByUrl('/members'),
+      error: error => this.toastr.error(error.error)
     })
   }
 
   logout() {
     this.accountService.logout();
     // this.loggedIn = false;
+    this.router.navigateByUrl('/');
   }
 }
