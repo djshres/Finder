3ecab5ce1e5217f7f1508e193956fb8795d197d0
Dheeraj Shrestha <dheeraj@crossovernepal.com>
2023-10-19 08:56:16 +0545

    Use of automapper

--- Finder/Controllers/UsersController.cs
+++ Finder/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
-﻿using Finder.Data;
+﻿using AutoMapper;
+using Finder.Data;
+using Finder.DTOs;
 using Finder.Entities;
 using Finder.Interfaces;
 using Microsoft.AspNetCore.Authorization;
@@ -13,23 +15,27 @@ namespace Finder.Controllers
     public class UsersController : ControllerBase
     {
         private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
 
-        public UsersController(IUserRepository userRepository)
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
             _userRepository = userRepository;
+            _mapper = mapper;
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
             var users = await _userRepository.GetUsersAsync();
-            return Ok(users);
+            var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
+            return Ok(usersToReturn);
         }
 
         [HttpGet("{username}")]
-        public async Task<ActionResult<AppUser>> GetUser(string username)
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
-            return await _userRepository.GetUserByUsernameAsync(username);
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+            return _mapper.Map<MemberDto>(user);
         }
     }
 }
---
+++ Finder/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Finder.DTOs;
+using Finder.Entities;
+
+namespace Finder.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser, MemberDto>();
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
